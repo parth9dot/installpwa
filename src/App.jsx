78a71eb646a25be41a26c0c484d7c38241b52e13
unknown <9dotparth@gmail.com>
2024-02-21 10:36:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,76 @@
+
+import { useEffect, useState } from 'react';
+import './App.css';
+const data = [
+  {
+    id:1,
+    user: 'user1',
+    amount: "20000",
+    status:true,
+    exitAmount:0
+  },
+  {
+    id:2,
+    user: 'user2',
+    amount: "30000",
+    status:true,
+    exitAmount:0
+  },
+  {
+    id:3,
+    user: 'user3',
+    amount: "40000",
+    status:true,
+    exitAmount:0
+  },
+  {
+    id:4,
+    user: 'user4',
+    amount: "50000",
+    status:true,
+    exitAmount:0
+  },
+  {
+    id:5,
+    user: 'user5',
+    amount: "60000",
+    status:true,
+    exitAmount:0
+  },
+  {
+    id:6,
+    user: 'user6',
+    amount: "700",
+    status:true,
+    exitAmount:0
+  },
+  {
+    id:7,
+    user: 'user7',
+    amount: "800",
+    status:true,
+    exitAmount:0
+  },
+  {
+    id:8,
+    user: 'user8',
+    amount: "900",
+    status:true,
+    exitAmount:0
+  },
+  {
+    id:9,
+    user: 'user9',
+    amount: "1000",
+    status:true,
+    exitAmount:0
+  },
+  {
+    id:10,
+    user: 'user10',
+    amount: "1100",
+    status:true,
+    exitAmount:0
+  }
+]
+export const demoData = data;
---
+++ src/AviatorGame.jsx
@@ -0,0 +1,154 @@
+import React, { useState, useEffect } from 'react';
+import { demoData } from './App';
+
+const AviatorGame = () => {
+    // const [altitude, setAltitude] = useState(1);
+    // const [crashed, setCrashed] = useState(false);
+    // const [bettingAmount, setBettingAmount] = useState(demoData)
+    // const [totalBetAmount, setTotalBetAmount] = useState(demoData.reduce((total, item) => total + parseInt(item.amount), 0))
+    // const [exitAmountTotal, setExitAmountTotal] = useState(0)
+    // const [liability, setLiability] = useState(0)
+
+    // // Simulate the plane's flight
+    // useEffect(() => {
+    //     const interval = setInterval(() => {
+    //         if (!crashed) {
+    //             setAltitude((prevAltitude) => (prevAltitude + 0.1));
+    //         }
+    //     }, 40);
+
+    //     return () => clearInterval(interval);
+    // }, [crashed]);
+
+    // const crashPlane = () => {
+    //     setCrashed(true)
+    //     const currentBalance = localStorage.getItem('wallet')
+    //     localStorage.setItem('wallet', parseFloat(currentBalance) + (totalBetAmount - exitAmountTotal))
+    //     setAltitude(1)
+    //     setExitAmountTotal(0)
+    //     setBettingAmount(demoData)
+    //     setTimeout(() => {
+    //         setCrashed(false)
+    //     }, 5000);
+    // }
+
+    // const exitUser = (id) => {
+    //     setBettingAmount(prevData => (
+    //         prevData.map(item => (
+    //             item.id === id ? { ...item, status: !item.status, exitAmount: item.amount * altitude } : item
+    //         ))
+    //     ))
+    // }
+
+    // // Handle crash
+    // useEffect(() => {
+    //     let amt = 0
+    //     bettingAmount.map(item => item.status ? amt = amt + (item.amount * altitude) : "")
+    //     setLiability(amt)
+    //     if (altitude > 10) {
+    //         crashPlane();
+    //     }
+    // }, [altitude]);
+
+    // useEffect(() => {
+    //     const total = bettingAmount.reduce((total, item) => total + parseInt(item.exitAmount), 0)
+    //     setExitAmountTotal(total)
+    //     console.log(bettingAmount)
+    // }, [bettingAmount])
+
+    const [supportsPWA, setSupportsPWA] = useState(false);
+    const [promptInstall, setPromptInstall] = useState(null);
+
+    useEffect(() => {
+        const handler = e => {
+          e.preventDefault();
+          console.log("we are being triggered :D");
+          setSupportsPWA(true);
+          setPromptInstall(e);
+        };
+        window.addEventListener("beforeinstallprompt", handler);
+    
+        return () => window.removeEventListener("transitionend", handler);
+      }, []);
+    const onClick = evt => {
+        evt.preventDefault();
+        if (!promptInstall) {
+          return;
+        }
+        promptInstall.prompt();
+      };
+    //   if (!supportsPWA) {
+    //     return null;
+    //   }
+
+
+    return (
+        <>
+            <div className="grid grid-cols-4 h-80">
+                {/* <div className="flex justify-center items-center bg-green-400">
+                    {
+                        crashed ? <>
+                            <h3 className='text-white text-2xl font-bold'>
+                                Plane Crashed
+                            </h3>
+                        </> :
+                            <h3 className='text-4xl font-bold'>{altitude.toFixed(2)} X</h3>
+                    }
+                </div>
+                <div>
+                    {
+                        !crashed && bettingAmount.map(item => {
+                            return (
+                                <div className="px-5 py-2 flex justify-between border-b-2 items-center" key={item.id}>
+                                    <h3 className='text-xl font-bold p-3'>{item.user}</h3>
+                                    <h3 className='text-xl font-bold p-3'>{!item.status && (item.exitAmount).toFixed(2)}</h3>
+                                    <h3 className='text-xl font-bold'>{item.status && (item.amount * altitude).toFixed()}</h3>
+                                    {
+                                        item.status &&
+                                        <button className='bg-red-500 p-3 font-bold text-white hover:bg-red-800 transition-all' onClick={e => exitUser(item.id)}>Exit Amount</button>
+                                    }
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+                <div className='m-3'>
+                    <div className="flex p-3 justify-between rounded-lg bg-green-300 my-3 px-5">
+                        <h2 className='text-xl font-bold'>Total Betting Amount :</h2>
+                        <h2 className='text-xl font-bold'>{totalBetAmount}</h2>
+                    </div>
+                    <div className="flex p-3 justify-between rounded-lg bg-green-300 my-3 px-5">
+                        <h2 className='text-xl font-bold'>Total Exit Amount :</h2>
+                        <h2 className='text-xl font-bold'>{bettingAmount.reduce((total, item) => total + parseInt(item.exitAmount), 0)}</h2>
+                    </div>
+                    <div className={`flex p-3 justify-between rounded-lg ${(totalBetAmount - exitAmountTotal) < 0 ? 'bg-red-300' : 'bg-green-300'} my-3 px-5`}>
+                        <h2 className='text-xl font-bold'>Total Profit Loss :</h2>
+                        <h2 className='text-xl font-bold'>{totalBetAmount - exitAmountTotal}</h2>
+                    </div>
+                    <div className="flex p-3 justify-between rounded-lg bg-green-300 my-3 px-5">
+                        <h2 className='text-xl font-bold'>Total Liablity :</h2>
+                        <h2 className='text-xl font-bold'>{liability.toFixed(2)}</h2>
+                    </div>
+                    <div className="flex p-3 justify-between rounded-lg bg-green-300 my-3 px-5">
+                        <h2 className='text-xl font-bold'>Total Balance :</h2>
+                        <h2 className='text-xl font-bold'>{localStorage.getItem('wallet')}</h2>
+                    </div>
+                </div> */}
+                <button className='p-3 bg-green-300' onClick={onClick}>Install App</button>
+            </div>
+            <div className="grid grid-cols-1 my-3">
+            </div>
+        </>
+        // <div>
+        //   <h1>Aviator Game</h1>
+        //   <p>Altitude: {altitude} meters</p>
+        //   {crashed ? (
+        //     <p>Plane crashed! 😢</p>
+        //   ) : (
+        //     <button onClick={() => setCrashed(true)}>Crash</button>
+        //   )}
+        // </div>
+    );
+};
+
+export default AviatorGame;
